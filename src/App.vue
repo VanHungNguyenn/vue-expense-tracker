d702bd22ee3dda2133a0782731f9c3ad355ac968
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2023-11-28 17:01:27 +0700

    interface

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<Header />
+	<div class="container">
+		<Balance />
+		<IncomeExpense />
+		<TransactionList :transactions="transactions" />
+		<AddTransaction />
+	</div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpense from './components/IncomeExpense.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
+import { reactive } from 'vue'
+
+const transactions = reactive([
+	{ id: 1, text: 'Flower', amount: -19.99 },
+	{ id: 2, text: 'Salary', amount: 300 },
+	{ id: 3, text: 'Book', amount: -10 },
+	{ id: 4, text: 'Camera', amount: 150 },
+])
+</script>
+
+<style scoped></style>
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,31 @@
+<template>
+	<h3>Add new transaction</h3>
+	<form action="" id="form">
+		<div class="form-control">
+			<label for="text">Text</label>
+			<input
+				type="text"
+				name="text"
+				id="text"
+				placeholder="Enter text..."
+			/>
+		</div>
+		<div class="form-control">
+			<label for="amount">
+				Amount <br />
+				(negative - expense, positive - income)
+			</label>
+			<input
+				type="number"
+				name="amount"
+				id="amount"
+				placeholder="Enter amount..."
+			/>
+		</div>
+		<button class="btn">Add transaction</button>
+	</form>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
